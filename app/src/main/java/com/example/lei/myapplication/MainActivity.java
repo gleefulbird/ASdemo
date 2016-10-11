b1ab5742763e5f1dbc66f49c3b9c2e0ba6149734
lei <gleefulbird@163.com>
2016-10-11 08:47:44 +0800

    谈吐是

--- app/src/main/java/com/example/lei/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lei/myapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.lei.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button btn = (Button) findViewById(R.id.button);
     }
+    public void startToast(View view){
+        Toast.makeText(this, "开始了", Toast.LENGTH_SHORT).show();
+    }
+    
 }
